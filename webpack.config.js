a80115ef7e1498eec8748ed8484374b2f7899697
Hyunchul Lee <cloudhc@gmail.com>
2018-06-23 10:39:40 +0900

    apply responsive web design and add favicon image.

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,7 @@ module.exports = {
         exclude: /node_modules/
       },
       {
-        test: /\.(png|jpg|gif|svg)$/,
+        test: /\.(png|jpg|gif|svg|ico)$/,
         use: {
           loader: 'url-loader',
           options: {
